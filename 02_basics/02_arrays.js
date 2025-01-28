3c6172eec7a55481e5174a1d8b9fc969fd4b81d7
Atulb0ra <159488436+Atulb0ra@users.noreply.github.com>
2025-01-28 14:20:27 +0000

    changes

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,7 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)//op: ["thor", "Ironman", "spiderman", "superman", "flash", "batman"]
 // console.log(allHeros);
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]//op: ["thor", "Ironman", "spiderman", "superman", "flash", "batman"]
+// const all_new_heros = [...marvel_heros, ...dc_heros]//spread method ->//op: ["thor", "Ironman", "spiderman", "superman", "flash", "batman"]
 
 // console.log(all_new_heros);
 
--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,7 +26,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = { obj1, obj2 } //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 // const obj5 = Object.assign({}, obj1, obj2, obj4)  //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
 
-// const obj6 = {...obj1, ...obj2} //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+// const obj6 = {...obj1, ...obj2}//spread method ---> //{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 // console.log(obj3);
 // console.log(obj5);
 // console.log(obj6);
