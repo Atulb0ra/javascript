7df046e358623ad0ab2484099c93b2d9cc6adcdd
Atulb0ra <159488436+Atulb0ra@users.noreply.github.com>
2025-01-29 00:08:29 +0000

    FUCTIONS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -55,7 +55,8 @@ function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-// handleObject(user)
+// handleObjecr //--->is just a reference
+// handleObject(user)//its a function call
 handleObject({//direct apssing object to function
     username: "sam",
     price: 399
